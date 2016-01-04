f8ccbdedd76f2706c876118aba5cde684f146b86
Abraham Lin <abraham_lin1996@berkeley.edu>
2016-01-04 14:45:08 -0800

    Added socket.io on client

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+/* io variable is similar to app variable. ie: app.get, app.post, app.delete etc */
+
 
 app.use(express.static(__dirname + '/public'));
+
+/* io.on allows us to listen for events */
+io.on("connection", function() {
+	console.log("User connected via socket.io");
+});
+
 http.listen(PORT, function() {
 	console.log("Server has started!");
 });
