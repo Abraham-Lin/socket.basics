562bb0bad929b9da854e32f84a5565cea0b7a65f
Abraham Lin <abraham_lin1996@berkeley.edu>
2016-01-04 15:42:20 -0800

    Able to parse through messages using jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,25 @@ var socket = io();
 socket.on("connect", function() {
 	console.log("Connected to socket.io server.");
 });
+
+/* This custom event "message" is what we specified in server.js */
+socket.on("message", function (message) {
+	console.log(message.text);
+});
+
+/* Handles submitting new messages */
+var $form = jQuery('#message-form'); /* we use jQuery to get everything with the form "message-form" */
+/* user submits the message */
+$form.on("submit", function (event) {
+	/* preventDefault is used on a form when you do not want to submit it the default way (refreshing) */
+	event.preventDefault();
+
+	var $message = $form.find("input[name=message]"); /* will search all input tags with name = message */
+	});
+
+	socket.emit("message", {
+		text: $message.val() /* .val() changes $message into a string */ 
+
+	/* we set the $message to an empty string */
+	$message.val("");
+});
--- server.js
+++ server.js
@@ -4,15 +4,23 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 /* io variable is similar to app variable. ie: app.get, app.post, app.delete etc */
-
-
+/* includes our public folder in this server.js file */
 app.use(express.static(__dirname + '/public'));
 
 /* io.on allows us to listen for event. This particular event checks forw hen a user has been connected. */
-io.on("connection", function() {
+io.on("connection", function (socket) {
 	console.log("User connected via socket.io");
+
+	socket.on("message", function (message) {
+		console.log("Message receieved: " + message.text);
+		socket.broadcast.emit('message', message); 
+	});
+
+	socket.emit("message", {
+		text: "Welcome to the chat application"
+	});
 });
 
-http.listen(PORT, function() {
+http.listen(PORT, function () {
 	console.log("Server has started!");
 });
